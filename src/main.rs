5a5d7982245e84f4dc0009fb9dbed274c065f3ed
Qiming Chu <cchuqiming@gmail.com>
2024-12-24 10:20:09 +0800

    bot: init bot framework

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,45 @@
+use dotenv::dotenv;
+use teloxide::prelude::*;
+
+use teloxide::utils::command::{BotCommands, ParseError};
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+pub enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    run().await
+}
+
+async fn run() {
+    dotenv().ok();
+    pretty_env_logger::init();
+    log::info!("Starting EminorBot...");
+
+    let bot = Bot::from_env();
+
+    let handler = dptree::entry().branch(
+        Update::filter_message()
+            .filter_command::<Command>()
+            .endpoint(answer),
+    );
+    Dispatcher::builder(bot, handler)
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+}
+
+pub async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    //TODO: Implement the command handler
+    Ok(())
 }
